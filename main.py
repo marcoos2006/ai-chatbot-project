27c96012c01c6373bf6ed08da0494c997da6475e
marcoos2006 <marcusseekw@gmail.com>
2024-08-07 20:23:58 +0800

    import

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 import os
 
-import s
+import streamlit as st
+from dotenv import load_dotenv
+import google.generativeai as gen_ai
+
+#load environment variables
+load_dotenv()
+
+#configure streamlit page settings
+st.set_page_config(
+    page_title="Chat with Gemini Pro",
+    page_icon="brain:",
+    layout="centered",
+)
